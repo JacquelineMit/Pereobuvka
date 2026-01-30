4e92a5cc19dea7da571bcc1c9f60d26d9d766f9b
jacqueline-vara02 <jacquelinevv@mail.ru>
2026-01-30 19:50:13 +0300

    js функции

--- js/index.js
+++ js/index.js
@@ -4,8 +4,15 @@ const textEl = document.querySelector('#text-container');
 let buttons = document.querySelectorAll('.btn');
 
 for (let i = 0; i < buttons.length; i++) {
-    const button = buttons[i]; // 0, 1, 2, 3
-    button.addEventListener("click", function() { 
+    const button = initButton(buttons[i]); // 0, 1, 2, 3
+    button.el.addEventListener("click", function() { 
+        if (button.isDisabled()) {
+            button.activate()
+        }
+        else {
+            button.disable()
+        }
+
         button.classList.toggle('btn--active');
         let newText = button.dataset.toggleText; //
         button.dataset.toggleText = button.innerHTML;
@@ -27,7 +34,6 @@ for (let i = 0; i < buttons.length; i++) {
 //         button.classList.toggle('btn--active')
 //     }) 
 // });
-
 
 /* типы в JS
 let boolean = true; // false
@@ -69,3 +75,53 @@ true + true; // -> 2
 (true + true) * (true + true) - true; // -> 3
 
 */
+
+
+function doSomething (value) {
+    function increment() {
+        value++
+        return value
+    }
+
+    function decrement() {
+        value--
+        return value;
+    }
+
+    function getValue() {
+        return value;
+    }
+
+    return {
+        increment, decrement, getValue
+    }
+}
+
+const res = doSomething(25);
+res.increment()
+res.increment()
+res.increment()
+console.log(res.getValue())
+
+function initButton(el) {
+
+    function disable() {
+        el.setAttribute('disabled', 'disabled');
+    }
+
+    function activate() {
+        el.removeAttribute('disabled');
+    }
+
+    function isDisabled() {
+        console.log(el.getAttribute('disabled'))
+        return el.getAttribute('disabled')
+    }
+
+    return {
+        disable,
+        activate,
+        isDisabled,
+        el
+    }
+}
